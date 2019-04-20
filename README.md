bdbfdfad5879f1dd6b06b5d9e0db5d6d934fa7fb
kaisa911 <lbf911@126.com>
2019-04-20 17:15:29 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # vue-webpack-todo
+
 imooc free course about VUE.JS &amp; webpack
+
+a todo application
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'src/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist')
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(gif|jpg|jepg|svg|png)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024,
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
